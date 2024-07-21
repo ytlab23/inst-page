fb357c38395401380a396032ec5f37e7d4ddfb26
Tinsel19 <olaoluwaoladunjoye130@gmail.com>
2024-07-21 14:11:23 +0100

    mobile response che ck

--- index.js
+++ index.js
@@ -2,11 +2,13 @@
 
 var menu = document.getElementById('menu');
 var changeImg = document.getElementById("changeImage");
+var navbar = document.getElementById('navBar');
 
 function changeMenu() {
     if (changeImg.getAttribute('src') === './main-menu.png')
         {
-            changeImg.src = './reject_12091676.png'
+            changeImg.src = './reject_12091676.png';
+            navbar.classList.add('open-menu')
         }
     else if (changeImg.getAttribute('src') === './reject_12091676.png') {
         changeImg.src = './main-menu.png'
