e6b4c513ff68e36f37e3dcc35f9af33f79c5596d
Tinsel19 <107304041+Tinsel19@users.noreply.github.com>
2024-07-24 13:54:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ var navbar = document.getElementById('navBar');
 function changeMenu() {
     if (changeImg.getAttribute('src') === './img/main-menu.png')
         {
-            changeImg.src = './reject_12091676.png';
+            changeImg.src = './img/reject_12091676.png';
             navbar.classList.remove('nav-bar');
             navbar.classList.add('open-menu');
         }
     else if (changeImg.getAttribute('src') === './reject_12091676.png') {
-        changeImg.src = './main-menu.png';
+        changeImg.src = './img/main-menu.png';
         navbar.classList.add('nav-bar');
             navbar.classList.remove('open-menu');
     }
